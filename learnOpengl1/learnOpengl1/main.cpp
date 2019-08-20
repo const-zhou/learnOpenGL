df71eda461270254ff64ab14c9c8183c945e0640
zhoudunguang <const_zhou@foxmail.com>
2019-08-20 17:15:02 +0800

    添加新的vao和ebo，切换不同的vao

--- learnOpengl1/learnOpengl1/main.cpp
+++ learnOpengl1/learnOpengl1/main.cpp
@@ -127,6 +127,36 @@ int main(int argc, const char * argv[]) {
     glBindVertexArray(0);
     
     
+    GLfloat vertices2[] = {
+        0.5f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f
+    };
+    GLuint indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+    GLuint VAO2, VBO2;
+    glGenVertexArrays(1, &VAO2);
+    glGenBuffers(1, &VBO2);
+    glBindVertexArray(VAO2);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+    
+    GLuint EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (GLvoid *)0);
+    glEnableVertexAttribArray(0);
+    
+    glBindVertexArray(0);
+    
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    
+    int i = 0;
     
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
@@ -135,9 +165,18 @@ int main(int argc, const char * argv[]) {
         glClear(GL_COLOR_BUFFER_BIT);
         
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        
+
+        if ((i / 10) % 2 == 0) {
+            glBindVertexArray(VAO2);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        } else {
+            glBindVertexArray(VAO);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+        }
+        i ++;
+        if (i > 100) {
+            i = 0;
+        }
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
